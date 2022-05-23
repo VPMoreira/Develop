4f6809355ebc26f08880fde701879abed1941d6e
VPMoreira <mateuscomput@gmail.com>
2022-05-23 14:52:55 -0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform, AlertController } from '@ionic/angular' ;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+constructor(
+private platform: Platform,
+private alertController: AlertController
+){
+  this.backButtonEvent();
+}
+
+backButtonEvent() {
+  this.platform.backButton.subscribeWithPriority(10, () => {
+    this.backButtonAlert();
+  });
+}
+
+async backButtonAlert() {
+  const alert = await this.alertController.create({
+    message: '!!!!!'
+  });
+
+  await alert.present();
+}
+
   title = 'Develop';
 }
--- src/app/shared/pedidos/pedidos.component.ts
+++ src/app/shared/pedidos/pedidos.component.ts
@@ -5,12 +5,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { ProdutosService } from '../services/produtos.service';
 
-const {App} = Plugins;
+const { App } = Plugins;
 
 StatusBar.setOverlaysWebView({ overlay: true });
 
 const setBackgroundColor = async () => {
-  await StatusBar.setBackgroundColor({color: '#006400'});
+  await StatusBar.setBackgroundColor({ color: '#006400' });
 }
 const showStatusBar = async () => {
   await StatusBar.show();
@@ -28,11 +28,11 @@ export interface IProdutos {
 }
 
 const ELEMENT_DATA: IProdutos[] = [
-  {id: '9CV267',orc: 845247,entrega: '6 de agosto de 2021',preco: 1280.56,descontos: 58.15,endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE',pagamento: 'Boleto - 14 dias',status: 'Pedido Recebido'},
-  {id: '9CV268',orc: 845248,entrega: '7 de agosto de 2021',preco: 1282.56,descontos: 58.25,endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE',pagamento: 'Boleto - 14 dias',status: 'Pedido Aprovado'},
-  {id: '9CV269',orc: 845249,entrega: '8 de agosto de 2021',preco: 1283.56,descontos: 58.30,endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE',pagamento: 'Boleto - 14 dias',status: 'Pedido Separado'},
-  {id: '9CV270',orc: 845250,entrega: '9 de agosto de 2021',preco: 1284.56,descontos: 58.35,endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE',pagamento: 'Boleto - 14 dias',status: 'Pedido em Transporte'},
-  {id: '9CV271',orc: 845251,entrega: '10 de agosto de 2021',preco: 1285.56,descontos: 58.40,endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE',pagamento: 'Boleto - 14 dias',status: 'Pedido Entregue'},
+  { id: '9CV267', orc: 845247, entrega: '6 de agosto de 2021', preco: 1280.56, descontos: 58.15, endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE', pagamento: 'Boleto - 14 dias', status: 'Pedido Recebido' },
+  { id: '9CV268', orc: 845248, entrega: '7 de agosto de 2021', preco: 1282.56, descontos: 58.25, endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE', pagamento: 'Boleto - 14 dias', status: 'Pedido Aprovado' },
+  { id: '9CV269', orc: 845249, entrega: '8 de agosto de 2021', preco: 1283.56, descontos: 58.30, endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE', pagamento: 'Boleto - 14 dias', status: 'Pedido Separado' },
+  { id: '9CV270', orc: 845250, entrega: '9 de agosto de 2021', preco: 1284.56, descontos: 58.35, endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE', pagamento: 'Boleto - 14 dias', status: 'Pedido em Transporte' },
+  { id: '9CV271', orc: 845251, entrega: '10 de agosto de 2021', preco: 1285.56, descontos: 58.40, endereco: 'QUARTO ANEL VIÁRIO, 2165 FORTALEZA/CE', pagamento: 'Boleto - 14 dias', status: 'Pedido Entregue' },
 ]
 
 
@@ -54,20 +54,21 @@ export class PedidosComponent implements OnInit {
   constructor(private productService: ProdutosService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
-    StatusBar.setBackgroundColor({color: '#006400'});
+    StatusBar.setBackgroundColor({ color: '#006400' });
 
-    this.router.params.subscribe( params => {
+    this.router.params.subscribe(params => {
       console.log(params)
       this.productService.getOne(params['id']).subscribe(response => {
         this.pedido = response
         console.log(this.pedido)
-  
-      
-    }
-
-    )})
 
 
+      }
 
-}
+      )
+    })
+
+
+
+  }
 }
